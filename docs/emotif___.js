2f670e4d67c451d6e2940e37c4b5c24f066560d8
Sean Batzel <romulus108@protonmail.com>
2017-09-26 17:01:47 -0400

    Closer... looks like [ and ] are giving me issues.

--- docs/emotif___.js
+++ docs/emotif___.js
@@ -10,96 +10,131 @@ function main() {
     console.log('Program is done.');
 }
 
+function parse_vector(program) {
+    program_vec = Array();
+    for (var i = 0, len = program.length; i < len; i++) {
+        switch (program[i]) {
+            case '>':
+                program_vec.push(1);
+                break;
+            case '<':
+                program_vec.push(2);
+                break;
+            case '-':
+                program_vec.push(3);
+                break;
+            case '+':
+                program_vec.push(4);
+                break;
+            case '.':
+                program_vec.push(5);
+                break;
+            case ',':
+                program_vec.push(6);
+                break;
+            case '[':
+                program_vec.push(7);
+                break;
+            case ']':
+                program_vec.push(8);
+                break;
+        }
+    }
+    return program_vec;
+}
+
 function compile(program) {
     var pc = 0;
-    var program_vec = program.split('');
+    var program_vec = parse_vector(program);
+    console.log("Program vector: " + program_vec);
     var instructions = new Array();
     var operands = new Array();
     var stack = new Array();
     for (x in program_vec) {
-        console.log(program_vec[x].charCodeAt(0));
-        switch (program_vec[x].charCodeAt(0)) {
-            case '🔥'.charCodeAt(0):
+        switch (program_vec[x]) {
+            case 1:
                 instructions.push(1);
                 operands.push(0);
                 break;
-            case '💯'.charCodeAt(0):
+            case 2:
                 instructions.push(2);
                 operands.push(0);
                 break;
-            case '💩'.charCodeAt(0):
+            case 3:
                 instructions.push(3);
                 operands.push(0);
                 break;
-            case '👍'.charCodeAt(0):
+            case 4:
                 instructions.push(4);
                 operands.push(0);
                 break;
-            case '💞'.charCodeAt(0):
+            case 5:
                 instructions.push(5);
                 operands.push(0);
                 break;
-            case '🙏'.charCodeAt(0):
+            case 6:
                 instructions.push(6);
                 operands.push(0);
                 break;
-            case '🌚'.charCodeAt(0):
+            case 7:
                 instructions.push(7);
                 operands.push(0);
                 stack.push(pc);
                 break;
-            case '🐸'.charCodeAt(0):
+            case 8:
                 var jmp_pc = stack.pop();
                 instructions.push(8);
                 operands.push(jmp_pc);
                 operands[jmp_pc] = pc;
             default:
-                pc -= 1;
+                pc--;
                 break;
         }
-        pc += 1;
+        pc++;
     }
     instructions.push([0,0]);
-    return [instructions, stack];
+    return [instructions, operands];
 }
 
 function interpret(state) {
-    var pc = 0;
     var ptr = 0;
     var data = new Array(1024);
+    for (var i = 0; i < 1024; i++) {
+        data[i] = 0;
+    }
     var output = "";
-    for (x in state[0]) {
-        switch (x) {
+    console.log("PC\tPTR\tDATA\tOP\tOPD")
+    for (var x = 0; x < state[0].length; x++) {
+        switch (state[0][x]) {
             case 0:
                 break;
             case 1: //right
-                ptr++;
+                ++ptr;
                 break;
             case 2: //left
-                ptr--;
+                --ptr;
                 break;
             case 3: //decrement
-                data[ptr]--;
+                --data[ptr];
                 break;
             case 4: //increment
-                data[ptr]++;
+                ++data[ptr];
                 break;
             case 5: //output
-                output = output + data[ptr];
+                output = output + String.fromCodePoint((data[ptr].toString()));
                 break;
             case 6: //input
-                console.log("Not implemented.");
                 break;
             case 7: //jmp_f
                 if (data[ptr] == 0) {
-                    pc = operands[pc];
+                    x = state[1][x];
                 }
             case 8: //jmp_bk
                 if (data[ptr] != 0) {
-                    pc = operands[pc];
+                    x = state[1][x];
                 }
         }
-        pc++;
+        console.log(x + "\t" + ptr + "\t" + data[ptr] + "\t" + state[0][x] + "\t" + state[1][x]);
     }
     return output;
 }
